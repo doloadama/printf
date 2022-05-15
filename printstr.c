b4e4ed1c9046b9ee5e747b60a59a61cf2b2d87ce
Adama Dolo <doloadama@outlook.com>
2022-05-15 21:59:21 +0000

    All functions to printf

--- printstr.c
+++ printstr.c
@@ -6,12 +6,12 @@
  * @format: format str.
  * Return: number str for print.
  */
+
 int printstr(char *format, va_list pa)
 {
 char *string = va_arg(pa, char *);
 int contador;
 (void)format;
-
 if (string == NULL)
 string = "(null)";
 contador = _puts(string);
